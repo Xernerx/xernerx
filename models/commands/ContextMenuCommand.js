0eb55a02048f693176f696843f289d6a4368c272
Dummi <19182@leerling.gitokbovenbouw.be>
2022-07-24 00:21:04 +0200

    V1.0.18 - Added version property and fixed context menu types.

--- models/commands/ContextMenuCommand.js
+++ models/commands/ContextMenuCommand.js
@@ -27,7 +27,8 @@ class ContextMenuCommand {
     }
 
     types(str) {
-        if (str?.toLowerCase() == "message" || str?.toLowerCase() == "user") str == 2 ? (str = 2) : (str = 3);
+        if (str?.toLowerCase() == "message") str = 2
+        if (str?.toLowerCase() == "user") str = 3
         return str
     }
 }
