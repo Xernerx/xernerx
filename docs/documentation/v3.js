2400608d326dda6867c8273ae4486c5cc0d7bc85
TheDummi <dummidevelopment@gmail.com>
2023-01-17 00:05:26 +0100

    Site Update

--- docs/documentation/v3.js
+++ docs/documentation/v3.js
@@ -3,6 +3,8 @@ export default {
         description: "The third version of Xernerx, coming in with TypeScript support and ESM.",
         deprecated: false,
         versions: [
+            "3.2.6",
+            "3.2.5",
             "3.2.2",
             "3.2.0",
             "3.1.4",
