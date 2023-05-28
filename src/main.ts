1bea5f275d85576b2af3c9e015cd6439f0f0ac4d
Dummi <raydecrom@hotmail.com>
2023-05-28 12:22:14 +0200

    Changed versioning

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+
 import XernerxClient from './client/XernerxClient.js';
 import MessageCommandBuilder from './build/MessageCommandBuilder.js';
 import SlashCommandBuilder from './build/SlashCommandBuilder.js';
@@ -6,14 +8,14 @@ import EventBuilder from './build/EventBuilder.js';
 import InhibitorBuilder from './build/InhibitorBuilder.js';
 import XernerxLog from './tools/XernerxLog.js';
 
-export default XernerxClient;
-
 export * from 'discord.js';
 export * from 'dumfunctions';
 import ExtensionBuilder from 'xernerx-extension-builder';
 
 import Discord from 'discord.js';
 
-const version = '4.2.2';
+const version = JSON.parse(fs.readFileSync('node_modules/xernerx/package.json', 'utf-8')).version;
+
+export default XernerxClient;
 
 export { XernerxClient, MessageCommandBuilder, SlashCommandBuilder, ContextCommandBuilder, EventBuilder, InhibitorBuilder, ExtensionBuilder, Discord, version, XernerxLog };
