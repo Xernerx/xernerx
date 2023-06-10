425731284c4fbe3d3e8a2560f1472aef1dc23748
TheDummi <raydegroot14@gmail.com>
2023-06-10 17:33:12 +0200

    added error thing for shardclient

--- src/client/XernerxShardClient.ts
+++ src/client/XernerxShardClient.ts
@@ -19,21 +19,25 @@ export default class XernerxShardClient extends ShardingManager {
         this.user = null;
 
         this.on('shardCreate', async (shard) => {
-            shard.on('ready', async () => {
-                const guilds = (await shard.fetchClientValue('guilds.cache')) as Array<Guild>;
-                const guildCount = guilds.length;
-                const userCount = guilds.map((guild) => guild.memberCount).length ? guilds.map((guild) => guild.memberCount).reduce((a, b) => (a += b)) : 0;
+            shard
+                .on('ready', async () => {
+                    const guilds = (await shard.fetchClientValue('guilds.cache')) as Array<Guild>;
+                    const guildCount = guilds.length;
+                    const userCount = guilds.map((guild) => guild.memberCount).length ? guilds.map((guild) => guild.memberCount).reduce((a, b) => (a += b)) : 0;
 
-                this.stats.guildCount += Number(guildCount);
+                    this.stats.guildCount += Number(guildCount);
 
-                this.stats.userCount += Number(userCount);
+                    this.stats.userCount += Number(userCount);
 
-                this.stats.shardCount++;
+                    this.stats.shardCount++;
 
-                this.user = (await shard.fetchClientValue('user')) as ClientUser;
+                    this.user = (await shard.fetchClientValue('user')) as ClientUser;
 
-                await new XernerxLog({ settings: xernerxOptions } as never).info(`Launched shard ${shard.id} for ${this.user?.tag}!`);
-            });
+                    await new XernerxLog({ settings: xernerxOptions } as never).info(`Launched shard ${shard.id} for ${this.user?.tag}!`);
+                })
+                .on('error', (error) => {
+                    new XernerxLog({ settings: xernerxOptions } as never).info(`An error occurred in ${shard.id} for ${this.user?.tag}! ${error}`);
+                });
         });
 
         this.spawn().then(async (shards) => {
