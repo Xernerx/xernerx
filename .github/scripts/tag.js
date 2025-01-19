16b67b3fe26996d9cfe27b475ce5ca5c2edf465b
Dummi <raydecrom@hotmail.com>
2025-01-19 11:38:10 +0100

    premium

--- .github/scripts/tag.js
+++ .github/scripts/tag.js
@@ -2,9 +2,9 @@
 
 import { exec } from 'child_process';
 
-import { promisify } from 'node:util';
-import pkg from '../../package.json' assert { type: 'json' };
+// import { promisify } from 'node:util';
+// import pkg from '../../package.json' assert { type: 'json' };
 
-const shell = promisify(exec);
+// const shell = promisify(exec);
 
-shell(`git tag ${pkg.version}`);
+// shell(`git tag ${pkg.version}`);
